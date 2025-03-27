d09deaffe6a863341286cf7f6655a2e95ddc5ea8
brostimi <tmadewusi@gmail.com>
2025-03-27 15:18:46 +0000

    Update add.ts

--- add.ts
+++ add.ts
@@ -1,21 +1,41 @@
-let nums: number[] = [2, 7, 11, 15];
-let target = 18;
+import promptSync from 'prompt-sync';
 
-function twoSum(nums: number[], target: number): [number, number]{
-    let numberToSearch = 0;
+const prompt = promptSync();
 
-    for(let num = 0; num < nums.length; num++){
-        if(nums[num] < target){
-            numberToSearch = target - nums[num]
+function getRandomInt(max: number)
+{
+    return Math.floor(Math.random() * max);
+}
 
-            for(let y = 0; y < nums.length; y++){
-                if(y != num && nums[y] == numberToSearch){
-                    return [num, y];
-                }
-            }
-        }
+function sum(number1: number, number2: number)
+{
+    return number1 + number2;
+}
+
+const questions: number = 5;
+let i: number = 1;
+let answersCorrect: number = 0;
+
+while(i <= questions)
+{
+    let num1 = getRandomInt(21);
+    let num2 = getRandomInt(21);
+
+    let sumOfNumbers: number = sum(num1, num2);
+
+    const question = prompt(num1 + " + " + num2 + ": ");
+    const answer = Number(question);
+
+    if(answer == sumOfNumbers)
+    {
+        console.log("Correct! The answer is " + sumOfNumbers);
+        answersCorrect++;
     }
-};
+    else
+    {
+        console.log("Incorrect! The answer is " + sumOfNumbers);
+    }
+    i++;
+}
 
-const numFunction = twoSum(nums, target);
-console.log(numFunction);
+console.log("You got " + answersCorrect + " questions correct!");
